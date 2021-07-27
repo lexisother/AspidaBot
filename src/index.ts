a0170ecec22fa11a74f39864ca3f11f68f60e30f
Alyxia Sother <lexisoth2005@gmail.com>
2021-07-27 11:14:40 +0200

    [Main] Added bare minimum

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello!");
+import * as discord from "discord.js";
+import config from "./config";
+
+const client = new discord.Client();
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user?.username}`);
+});
+
+client.login(config.token);
